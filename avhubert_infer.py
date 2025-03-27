81936bab1345d42ba288f452eeb67edef9da17e5
Bandhav Veluri <bandhav.veluri00@gmail.com>
2025-03-27 16:44:26 -0700

    inference_script

--- avhubert_infer.py
+++ avhubert_infer.py
@@ -15,7 +15,7 @@ FFMPEG = '/mmfs1/gscratch/cse/bandhav/miniconda3/envs/avhubert/bin/ffmpeg'
 os.environ['PATH'] = f'{os.path.dirname(FFMPEG)}:' + os.environ['PATH']
 
 root = git.Repo('.', search_parent_directories=True).working_tree_dir
-work_dir = os.path.join(root, 'modules', 'av_hubert', 'avhubert')
+work_dir = os.path.join(root, 'modules', 'AVHuBERT', 'avhubert')
 sys.path.insert(0, work_dir)
 
 from preparation.align_mouth import landmarks_interpolate, crop_patch, write_video_ffmpeg
@@ -54,9 +54,7 @@ def preprocess_video(input_video_path, output_video_path, face_predictor_path, m
     return
 
 
-def extract_visual_features_from_roi(video_path, ckpt_path, user_dir):
-    utils.import_user_module(Namespace(user_dir=user_dir))
-    models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
+def extract_visual_features_from_roi(video_path, models, task):
     transform = avhubert_utils.Compose([
         avhubert_utils.Normalize(0.0, 255.0),
         avhubert_utils.CenterCrop((task.cfg.image_crop_size, task.cfg.image_crop_size)),
@@ -78,7 +76,7 @@ def extract_visual_features_from_roi(video_path, ckpt_path, user_dir):
 
 
 def extract_visual_features_from_video(
-    origin_clip_path, face_predictor_path, mean_face_path, ckpt_path, user_dir
+    origin_clip_path, face_predictor_path, mean_face_path, models, task
 ):
     # Create a temporary file for mouth_roi_path
     with tempfile.NamedTemporaryFile(suffix='.mp4') as mouth_roi:
@@ -88,7 +86,7 @@ def extract_visual_features_from_video(
         preprocess_video(origin_clip_path, mouth_roi_path, face_predictor_path, mean_face_path)
 
         # Call the feature extraction function
-        feature = extract_visual_features_from_roi(mouth_roi_path, ckpt_path, user_dir)
+        feature = extract_visual_features_from_roi(mouth_roi_path, models, task)
 
     # Return the feature variable
     return feature
@@ -98,7 +96,9 @@ if __name__ == '__main__':
     face_predictor_path = f"{root}/data/misc/shape_predictor_68_face_landmarks.dat"
     mean_face_path = f"{root}/data/misc/20words_mean_face.npy"
     ckpt_path = f"{root}/data/checkpoints/base_vox_433h.pt"
-    user_dir = f"{root}/modules/av_hubert/avhubert"
 
-    features = extract_visual_features_from_roi(f'roi.mp4', ckpt_path, user_dir)
+    utils.import_user_module(Namespace(user_dir=work_dir))
+    models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
+
+    features = extract_visual_features_from_roi(f'roi.mp4', models, task)
     print(features.shape)
