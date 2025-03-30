26a3ca07bf245a11b383a21eb813de71dda99927
Bandhav Veluri <bandhav.veluri00@gmail.com>
2025-03-29 18:18:57 -0700

    setup

--- README.md
+++ README.md
@@ -8,27 +8,5 @@
 
 2. Create env
 
-        conda create -n avhubert_gpu python=3.8 -y
-        conda activate avhubert_gpu
-        conda install pytorch==1.10.0 torchaudio==0.10.0 cudatoolkit=10.2 -c pytorch
-        conda install conda-forge::ffmpeg
-
-3. Downgrade `pip` to 23.1.2
-
-        python -m pip install --upgrade pip==23.1.2
-
-4. Install `fairseq` and `avhubert` dependencies
-
-        pip install -r requirements.txt
-        cd modules/avhubert/fairseq
-        pip install --editable .
-        cd ../../..
-
-5. Install `dlib`
-
-        ./install_dlib.sh
-
-6. Obtain checkpoints
-
-        cd ../../..
-        ./download.sh
+        module load cuda/11.3.0 # Make sure cuda is available
+        ./setup.sh
--- setup.sh
+++ setup.sh
@@ -2,35 +2,27 @@
 
 set -e
 
-# Clone submodules
-git submodule update --init --recursive
-
-# Create conda environment
-conda create -n avhubert_gpu python=3.8 -y
-conda activate avhubert_gpu
-
 # Install torch and ffmpeg
-conda install pytorch==1.10.0 torchaudio==0.10.0 cudatoolkit=10.2 -c pytorch
-conda install conda-forge::ffmpeg
+conda install -y conda-forge::ffmpeg
 
-# # Install pip and requirements
-# python -m pip install --upgrade pip==23.1.2
-# pip install -r requirements.txt
+# Install pip and requirements
+python -m pip install --upgrade pip==23.1.2
+pip install -r requirements.txt
 
-# # Install fairseq
-# cd modules/avhubert/fairseq
-# pip install --editable .
-# cd ../../..
+# Install fairseq
+cd modules/AVHuBERT/fairseq
+pip install --editable .
+cd ../../..
 
-# # Install dlib
-# cd modules/dlib
-# mkdir -p build
-# cd build
-# cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1
-# cmake --build .
-# cd ..
-# python setup.py install --set DLIB_USE_CUDA=1
-# cd ../..
+# Install dlib
+cd modules/dlib
+mkdir -p build
+cd build
+cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1
+cmake --build .
+cd ..
+python setup.py install --set DLIB_USE_CUDA=1
+cd ../..
 
-# # Download pre-trained models
-# ./download.sh
+# Download pre-trained models
+./download.sh
