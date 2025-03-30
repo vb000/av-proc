3aa05a2303bf19a9c54da165238f58795bf5b035
Bandhav Veluri <bandhav.veluri00@gmail.com>
2025-03-30 01:02:10 -0700

    cnn_face_detector

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 mkdir -p data/misc
+wget https://dlib.net/files/mmod_human_face_detector.dat.bz2 -O data/misc/mmod_human_face_detector.dat.bz2
+bzip2 -d data/misc/mmod_human_face_detector.dat.bz2
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 -O data/misc/shape_predictor_68_face_landmarks.dat.bz2
 bzip2 -d data/misc/shape_predictor_68_face_landmarks.dat.bz2
 wget --content-disposition https://github.com/mpc001/Lipreading_using_Temporal_Convolutional_Networks/raw/master/preprocessing/20words_mean_face.npy -O data/misc/20words_mean_face.npy
