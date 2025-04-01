b614c7f2c18f1ff2cb87a1819d7fb95403e8b359
Bandhav Veluri <bandhav.veluri00@gmail.com>
2025-04-01 00:21:56 -0700

    vsr

--- avhubert_infer.py
+++ avhubert_infer.py
@@ -20,7 +20,9 @@ sys.path.insert(0, work_dir)
 
 from preparation.align_mouth import landmarks_interpolate, crop_patch, write_video_ffmpeg
 import utils as avhubert_utils
+import fairseq
 from fairseq import checkpoint_utils, options, tasks, utils
+from fairseq.dataclass.configs import GenerationConfig
 
 
 def detect_landmark(image, detector, predictor, cnn_detector=False):
@@ -103,6 +105,62 @@ def extract_visual_features_from_video(
     return feature
 
 
+def visual_speech_recognition_from_roi(video_path, models, saved_cfg, task):
+    num_frames = int(cv2.VideoCapture(video_path).get(cv2.CAP_PROP_FRAME_COUNT))
+    data_dir = tempfile.mkdtemp()
+    tsv_cont = ["/\n", f"test-0\t{video_path}\t{None}\t{num_frames}\t{int(16_000*num_frames/25)}\n"]
+    label_cont = ["DUMMY\n"]
+    with open(f"{data_dir}/test.tsv", "w") as fo:
+        fo.write("".join(tsv_cont))
+    with open(f"{data_dir}/test.wrd", "w") as fo:
+        fo.write("".join(label_cont))
+
+    modalities = ["video"]
+    gen_subset = "test"
+    gen_cfg = GenerationConfig(beam=20)
+    models = [model.eval().cuda() for model in models]
+
+    saved_cfg.task.modalities = modalities
+    saved_cfg.task.data = data_dir
+    saved_cfg.task.label_dir = data_dir
+    task = tasks.setup_task(saved_cfg.task)
+    task.load_dataset(gen_subset, task_cfg=saved_cfg.task)
+    generator = task.build_generator(models, gen_cfg)
+
+    def decode_fn(x):
+        dictionary = task.target_dictionary
+        symbols_ignore = generator.symbols_to_strip_from_output
+        symbols_ignore.add(dictionary.pad())
+        return task.datasets[gen_subset].label_processors[0].decode(x, symbols_ignore)
+
+    itr = task.get_batch_iterator(dataset=task.dataset(gen_subset)).next_epoch_itr(shuffle=False)
+    sample = next(itr)
+    sample = utils.move_to_cuda(sample)
+    hypos = task.inference_step(generator, models, sample)
+    ref = decode_fn(sample['target'][0].int().cpu())
+    hypo = hypos[0][0]['tokens'].int().cpu()
+    hypo = decode_fn(hypo)
+
+    return hypo
+
+
+def visual_speech_recognition(
+    video_path, face_predictor_path, mean_face_path, models, saved_cfg, task, cnn_detector_path=None
+):
+    with tempfile.NamedTemporaryFile(suffix='.mp4') as mouth_roi:
+        mouth_roi_path = mouth_roi.name
+
+        # Call the preprocess_video function
+        preprocess_video(
+            video_path, mouth_roi_path, face_predictor_path, mean_face_path,
+            cnn_detector_path=cnn_detector_path
+        )
+
+        hypo = visual_speech_recognition_from_roi(mouth_roi_path, models, saved_cfg, task)
+
+    return hypo
+
+
 if __name__ == '__main__':
     face_predictor_path = f"{root}/data/misc/shape_predictor_68_face_landmarks.dat"
     mean_face_path = f"{root}/data/misc/20words_mean_face.npy"
@@ -112,12 +170,23 @@ if __name__ == '__main__':
     utils.import_user_module(Namespace(user_dir=work_dir))
     models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
 
-    features = extract_visual_features_from_video(
-        origin_clip_path=f"{root}/data/misc/avhubert_demo_video_8s.mp4",
+    # features = extract_visual_features_from_video(
+    #     origin_clip_path=f"{root}/data/misc/avhubert_demo_video_8s.mp4",
+    #     face_predictor_path=face_predictor_path,
+    #     mean_face_path=mean_face_path,
+    #     models=models,
+    #     task=task,
+    #     cnn_detector_path=cnn_detector_path
+    # )
+    # print(features.shape) # [seq_len, 768]
+
+    hypo = visual_speech_recognition(
+        video_path=f"{root}/data/misc/avhubert_demo_video_8s.mp4",
         face_predictor_path=face_predictor_path,
         mean_face_path=mean_face_path,
         models=models,
+        saved_cfg=saved_cfg,
         task=task,
         cnn_detector_path=cnn_detector_path
     )
-    print(features.shape) # [seq_len, 768]
+    print(hypo)
